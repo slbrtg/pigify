10e7f57683c40e2a73838e7d679891e9bd74f9eb
Diego Aguilar and Saul Artiga <diego+saul@>
2017-06-07 13:11:09 -0700

    develop working pig latin js

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,27 @@
-//backend logic
 
-
-
-//jQuery functionality
 $(document).on("submit", "form#word-input", function(){
   event.preventDefault();
-
-  var vowels = "aeiou";
-  var pigWord = "";
   var str = $("#input-word").val();
+  var vowel = "aeiouq";
+  var match = -1;
+  var index = -1;
+  var indexQ
 
-  for (x = 0; x < str.length; x++){
-    var letter = str.charAt(x);
-  	for ( var y = 0; y < vowels.length; y++){
-    	if (letter === vowels.charAt(y)){
-      	letter = "-";
-      }
+  // find index of first vowel
+  for (var x = 0; x < str.length && match < 0; x++) {
+    match = vowel.indexOf(str.charAt(x));
+    if (match >= 0) {
+      index = x;
+      indexQ = index + 2;
     }
-    pigWord += letter;
   }
-  console.log(pigWord);
-
-
-
-  $("#output").append("<h3 id='pig-word'>"+pigWord+"</h3>");
-});
-
-
-$(document).ready(function() {
-
+  if (str.charAt(index) === 'q'){
+    str = str.slice(indexQ)  + str.substring(0, indexQ) +  "ay";
+  } else if (index > 0) {
+    str = str.slice(index) + str.substring(0, index) + "ay";
+  } else {
+    str = str.slice(index) + str.substring(0, index) + "way";
+  }
+  $("#output").empty();
+  $("#output").append("<h3 id='pig-word'>"+str+"</h3>");
 });
