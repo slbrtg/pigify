57c33fdf77a76ba91a4c4b078443ebe4cc384290
Diego Aguilar and Saul Artiga <diego+saul@>
2017-06-07 15:25:08 -0700

    further develop backend js to handle multiple words

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,37 @@
 $(document).on("submit", "form#word-input", function(){
   event.preventDefault();
   var str = $("#input-word").val();
+  var pigStr = "";
+  var ogStrArray = str.split(" ");
+  var newStrArray = [];
   var vowel = "aeiouq";
   var match = -1;
   var index = -1;
   var indexQ;
 
   // find index of first vowel
-  for (var x = 0; x < str.length && match < 0; x++) {
-    match = vowel.indexOf(str.charAt(x));
-    if (match >= 0) {
-      index = x;
-      indexQ = index + 2;
-    }
-  }
-  if (str.charAt(index) === 'q'){
-    str = str.slice(indexQ)  + str.substring(0, indexQ) +  "ay";
-  } else if (index > 0) {
-    str = str.slice(index) + str.substring(0, index) + "ay";
-  } else {
-    str = str.slice(index) + str.substring(0, index) + "way";
-  }
+    ogStrArray.forEach(function(element) {
+      for (var x = 0; x < element.length && match < 0; x++) {
+        console.log(element);
+        match = vowel.indexOf(element.charAt(x));
+        if (match >= 0) {
+          index = x;
+          indexQ = index + 2;
+        }
+      }
+      if (element.charAt(index) === 'q'){
+        element = element.slice(indexQ)  + element.substring(0, indexQ) +  "ay";
+      } else if (element.charAt(index) === 'q'){
+        element = element.slice(indexQ)  + element.substring(0, indexQ) +  "ay";
+      } else if (index > 0) {
+        element = element.slice(index) + element.substring(0, index) + "ay";
+      } else {
+        element = element.slice(index) + element.substring(0, index) + "way";
+      }
+      newStrArray.push(element);
+    });
+  console.log(newStrArray);
+  pigStr = newStrArray.join(" ");
   $("#output").empty();
-  $("#output").append("<h3 id='pig-word'>"+str+"</h3>");
+  $("#output").append("<h3 id='pig-word'>"+pigStr+"</h3>");
 });
