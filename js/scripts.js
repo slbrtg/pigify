6a1392289a6829a1bdae06febe2fcc9ec18f205e
Diego Aguilar and Saul Artiga <diego+saul@>
2017-06-07 09:42:01 -0700

    create draft of html elements and js backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
+//backend logic
+
+
+
+//jQuery functionality
+$(document).on("submit", "form#word-input", function(){
+  event.preventDefault();
+
+  var vowels = "aeiou";
+  var pigWord = "";
+  var str = $("#input-word").val();
+
+  for (x = 0; x < str.length; x++){
+    var letter = str.charAt(x);
+  	for ( var y = 0; y < vowels.length; y++){
+    	if (letter === vowels.charAt(y)){
+      	letter = "-";
+      }
+    }
+    pigWord += letter;
+  }
+  console.log(pigWord);
+
+
+
+  $("#output").append("<h3 id='pig-word'>"+pigWord+"</h3>");
+});
+
+
 $(document).ready(function() {
-  
+
 });
